86e64cffe24c3acd2c83fbee83659836a7fcf63e
Erno Viitanen <erno.viitanen@student.lut.fi>
2023-05-09 14:50:16 +0300

    gif and low resolution

--- encode.sh
+++ encode.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+#brew install ffmpeg
+#brew install imagemagick
 
 function handleopts {
   while test "$1" != ""; do
@@ -7,22 +9,24 @@ function handleopts {
         -i|--input) INPUT_FILE=$2; shift ;;
         -o|--output) OUTPUT_FILE=$2; shift ;;
         -s|--speed) SPEED_FACTOR=$2; shift ;;
+        -g|--gif) GIF_FILE=true; shift ;;
+        -l|--low) LOW_RESOLUTION=true; shift ;;
         -te|--trimend) TRIM_EOF_DURATION=$2; shift ;;
-        -t|--threads) NUMBER_OF_THREADS=$2; shift ;; 
+        -t|--threads) NUMBER_OF_THREADS=$2; shift ;;
         * )
-          #if test "$1" != "$cmd" &&          
+          #if test "$1" != "$cmd" &&
             # test "$1" != "$author"; then
            # echo " $1"
           #fi
           shift
           ;;
-       
+
     esac
   done
 }
 
 SPEED_FACTOR=1.5 # Default is 1.5 X speed
-NUMBER_OF_THREADS=4 # Default is 4 threads
+NUMBER_OF_THREADS=8 # Default is 4 threads
 TRIM_EOF_DURATION=0 # Default is 0.0 second trimmed from EOF
 INPUT_FILE=""
 OUTPUT_FILE=""
@@ -33,7 +37,7 @@ RECORDINGS_DIRECTORY="/Users/viitanener/Pictures/Screenshots" # predefined direc
 
 # when no input file, use last recording
 if test "$INPUT_FILE" == ""; then
-   LAST_RECORDING=$(ls -1t $RECORDINGS_DIRECTORY/*.mov | head -1) 
+   LAST_RECORDING=$(ls -1t $RECORDINGS_DIRECTORY/*.mov | head -1)
 else
    LAST_RECORDING=$INPUT_FILE
 fi
@@ -48,7 +52,12 @@ FILENAME_ONLY="${FILENAME_EXT%.*}"
 EXT_ONLY="${FILENAME_EXT##*.}" # Or hardcode it like "mp4"
 FILENAME_ONLY_PATH="${BASE_PATH}/${FILENAME_ONLY}"
 
-OUTPUT_FILE_EXT="${FILENAME_ONLY_PATH}.mp4"
+
+if [ $GIF_FILE ]; then
+  OUTPUT_FILE_EXT="${FILENAME_ONLY_PATH}.gif"
+else
+  OUTPUT_FILE_EXT="${FILENAME_ONLY_PATH}.mp4"
+fi
 
 INPUT_DURATION=$(ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 "${FILENAME_ONLY_PATH}.${EXT_ONLY}")
 INPUT_DURATION=$(bc <<< "$INPUT_DURATION"/"$SPEED_FACTOR")
@@ -66,8 +75,21 @@ echo "FILENAME_ONLY_PATH $FILENAME_ONLY_PATH"
 echo "OUTPUT_FILE $OUTPUT_FILE_EXT"
 echo "INPUT/OUTPUT_DURATION $INPUT_DURATION/$OUTPUT_DURATION"
 
-ffscript="ffmpeg -i $FILENAME_NO_SPACES -r 23.976 -threads $NUMBER_OF_THREADS -t '$OUTPUT_DURATION' -vf 'setpts=(PTS-STARTPTS)/${SPEED_FACTOR}' -af atempo=$SPEED_FACTOR $OUTPUT_FILE_EXT"
+if [ $GIF_FILE ]; then
+  if [ $LOW_RESOLUTION ]; then
+    ffscript="ffmpeg -i $FILENAME_NO_SPACES -threads $NUMBER_OF_THREADS -t '$OUTPUT_DURATION' -vf 'setpts=(PTS-STARTPTS)/${SPEED_FACTOR}' -af atempo=$SPEED_FACTOR -pix_fmt rgb24 -r 5 -f gif $OUTPUT_FILE_EXT"
+  else
+    ffscript="ffmpeg -i $FILENAME_NO_SPACES -r 23.976 -threads $NUMBER_OF_THREADS -t '$OUTPUT_DURATION' -vf 'setpts=(PTS-STARTPTS)/${SPEED_FACTOR}' -af atempo=$SPEED_FACTOR $OUTPUT_FILE_EXT && gifsicle -O3 $OUTPUT_FILE_EXT -o $OUTPUT_FILE_EXT"
+  fi
+else
+  ffscript="ffmpeg -i $FILENAME_NO_SPACES -r 23.976 -threads $NUMBER_OF_THREADS -t '$OUTPUT_DURATION' -vf 'setpts=(PTS-STARTPTS)/${SPEED_FACTOR}' -af atempo=$SPEED_FACTOR $OUTPUT_FILE_EXT"
+fi
+echo "\n-----------------------------------------------------------------------------------"
 echo "$ffscript"
+echo "-----------------------------------------------------------------------------------\n"
 eval $ffscript
-terminal-notifier -message "Encoding complete" -execute "open $RECORDINGS_DIRECTORY"
-
+retVal=$?
+if [ $retVal -eq 0 ]; then
+  terminal-notifier -message "Encoding complete" -execute "open $RECORDINGS_DIRECTORY"
+  echo "\nDone. $OUTPUT_FILE_EXT"
+fi
